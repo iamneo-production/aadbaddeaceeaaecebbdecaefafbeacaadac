2fa67feb1c48d2a44cc0b9da78f4a23096dad04d
Iamneo <techadmin@iamneo.ai>
2023-09-08 12:24:55 +0000

    Backup Commit

--- demo/src/main/java/com/jdbc/App.java
+++ demo/src/main/java/com/jdbc/App.java
@@ -1,13 +1,37 @@
-package com.jdbc;
+package demo.src.main.java.com.jdbc;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static String mySqlDriver;
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+    
+    static {
+      
+        App.URL = "jdbc:mysql://localhost:3306/appDB?useSSL=false";
+        App.USERNAME = "root";
+        App.PASSWORD = "examly";
+    }
+    
+    public static void main(String[] args)  {
+         Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(App.URL, App.USERNAME, App.PASSWORD);
+            if (conn != null) {
+                System.out.println("Connection SUCCESSFULL.");
+            }
+            else {
+                System.out.println("Connection FAILED......Try Again");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 }
