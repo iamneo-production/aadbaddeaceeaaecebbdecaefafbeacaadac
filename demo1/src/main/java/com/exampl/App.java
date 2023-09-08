208dd74062540dd67c131ad69b328c6311b448c3
Iamneo <techadmin@iamneo.ai>
2023-09-08 12:29:54 +0000

    Backup Commit

---
+++ demo1/src/main/java/com/exampl/App.java
@@ -0,0 +1,13 @@
+package demo1.src.main.java.com.exampl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "################################3Hello World!***********************************************" );
+    }
+}
