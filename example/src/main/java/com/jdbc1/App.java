ddd976efc1bd14cf68ebff8bac467254a87b1304
Iamneo <techadmin@iamneo.ai>
2023-09-12 09:58:51 +0000

    Backup Commit

--- example/src/main/java/com/jdbc1/App.java
+++ example/src/main/java/com/jdbc1/App.java
@@ -10,27 +10,27 @@ import java.sql.DriverManager;
  *
  */
 public class App 
-{private static String mySqlDriver;
+{
     private static String URL;
     private static String USERNAME;
     private static String PASSWORD;
     
     static {
        
-        DBConnectionUtility.URL = "jdbc:mysql://localhost:3306/appDB?useSSL=false";
-        DBConnectionUtility.USERNAME = "root";
-        DBConnectionUtility.PASSWORD = "examly";
+        URL = "jdbc:mysql://localhost:3306/appDB?useSSL=false";
+        USERNAME = "root";
+        PASSWORD = "examly";
     }
     
     public static void main(String[] args)  {
          Connection conn = null;
         try {
-            conn = DriverManager.getConnection(DBConnectionUtility.URL, DBConnectionUtility.USERNAME, DBConnectionUtility.PASSWORD);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             if (conn != null) {
-                System.out.println("Connection SUCCESSFULL.");
+                System.out.println("#################################Connection SUCCESSFULL.");
             }
             else {
-                System.out.println("Connection FAILED......Try Again");
+                System.out.println("##############################Connection FAILED......Try Again");
             }
         }
         catch (Exception e) {
