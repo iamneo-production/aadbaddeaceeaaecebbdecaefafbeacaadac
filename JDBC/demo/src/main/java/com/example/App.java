3f5c1515d9fd1592b91216b843657c65af5442ed
Iamneo <techadmin@iamneo.ai>
2023-09-12 10:29:44 +0000

    Backup Commit

--- JDBC/demo/src/main/java/com/example/App.java
+++ JDBC/demo/src/main/java/com/example/App.java
@@ -1,32 +1,89 @@
+
+
 package com.example;
 
+ 
+
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {Connection conn = null;
+import java.sql.SQLException;
+
+import java.sql.Statement;
+
+ 
+
+public class App {
+
+    public static void main(String[] args) {
+
+       String jdbcURL = "jdbc:mysql://localhost:3306/appdb";
+
+        String username = "root";
+
+        String password = "examly";
+
+ 
+
         try {
-            String URL = "jdbc:mysql://localhost:3306/appDB?useSSL=false";
-            String USERNAME = "root";
-             String PASSWORD = "examly";
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (conn != null) {
-                System.out.println("#################################Connection SUCCESSFULL.");
-            }
-            else {
-                System.out.println("##############################Connection FAILED......Try Again");
-            }
-        }
-        catch (Exception e) {
+
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+
+            System.out.println("Connected to the database.");          
+
+            Statement statement = connection.createStatement();
+
+ 
+
+            // Create a table
+
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS example_table (" +
+
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+
+                    "name VARCHAR(255)," +
+
+                    "age INT)";
+
+            statement.execute(createTableSQL);
+
+            System.out.println("Table created successfully.");
+
+ 
+
+            // Insert values into the table
+
+            String insertSQL = "INSERT INTO example_table (name, age) VALUES " +
+
+                    "('Priya', 27), " +
+
+                    "('Yuvaraj', 33)";
+
+            int rowsAffected = statement.executeUpdate(insertSQL);
+
+            System.out.println(rowsAffected + " row(s) inserted.");
+
+ 
+
+            // Close the Statement and Connection
+
+            statement.close();
+
+            connection.close();
+
+            System.out.println("Connection closed.");
+
+   
+
+        } catch (SQLException e) {
+
             e.printStackTrace();
+
         }
-        
-    
+
+ 
+
     }
+
 }
