4a2123cc8e0082b5615b8e2fb9a16c35ee745486
Iamneo <techadmin@iamneo.ai>
2023-09-13 08:33:17 +0000

    Backup Commit

--- JDBC/crud/src/main/java/com/jdbc/utility/DBConnectionUtility.java
+++ JDBC/crud/src/main/java/com/jdbc/utility/DBConnectionUtility.java
@@ -6,38 +6,22 @@ import java.sql.Connection;
 
 public class DBConnectionUtility
 {
-    private static String mySqlDriver;
     private static String URL;
     private static String USERNAME;
     private static String PASSWORD;
     
     static {
        
-        DBConnectionUtility.URL = "jdbc:mysql://localhost:3306/appDB?useSSL=false";
+        DBConnectionUtility.URL = "jdbc:mysql://localhost:3306/empDB?useSSL=false";
         DBConnectionUtility.USERNAME = "root";
         DBConnectionUtility.PASSWORD = "examly";
     }
     
-    public static void main(String[] args)  {
-         Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(DBConnectionUtility.URL, DBConnectionUtility.USERNAME, DBConnectionUtility.PASSWORD);
-            if (conn != null) {
-                System.out.println("Connection SUCCESSFULL.");
-            }
-            else {
-                System.out.println("Connection FAILED......Try Again");
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-    }
+
     public static  Connection getConnection() throws Exception {
         Connection conn = null;
         try {
-            Class.forName(DBConnectionUtility.mySqlDriver);
+        
             conn = DriverManager.getConnection(DBConnectionUtility.URL, DBConnectionUtility.USERNAME, DBConnectionUtility.PASSWORD);
             if (conn != null) {
                 System.out.println("Connection SUCCESSFULL.");
